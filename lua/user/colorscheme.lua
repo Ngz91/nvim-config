c2a813238911009b922e54377317d4dbd8ebf1d3
Nevio <neviogomez91@gmail.com>
2022-06-23 21:04:37 -0400

    Initial commit

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,11 @@
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+
+vim.cmd [[
+try
+  colorscheme tokyonight
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
