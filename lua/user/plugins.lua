da81db0d423efa4bbbe8adab6632473347b605bf
Nevio <neviogomez91@gmail.com>
2022-07-19 17:47:55 -0400

    Added neoscroll and nvim-colorizer for testing

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,6 +58,8 @@ return packer.startup(function(use)
   use "goolord/alpha-nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
+  use "karb94/neoscroll.nvim"
+  use "NvChad/nvim-colorizer.lua"
 
   use {
     "SmiteshP/nvim-gps",
