9d040a5c53536b42c873e9aab8a4ddd6a833b933
Nevio <neviogomez91@gmail.com>
2022-07-19 21:51:50 -0400

    Changed neotest config

--- lua/user/neotest.lua
+++ lua/user/neotest.lua
@@ -3,14 +3,71 @@ if not status_ok then
     return
 end
 
-require("neotest").setup({
+local M = {}
+
+function M.post()
+  local neotest = require("neotest")
+  neotest.setup({
+    icons = {
+      running = "↻",
+    },
+    status = {
+      virtual_text = true,
+      signs = false,
+    },
+    strategies = {
+      integrated = {
+        width = 180,
+      },
+    },
     adapters = {
       require("neotest-python")({
-        dap = { justMyCode = false },
+        dap = { justMyCode = false, console = "integratedTerminal" },
       }),
       require("neotest-plenary"),
       require("neotest-vim-test")({
-        ignore_file_types = { "python", "vim", "lua" },
+        allow_file_types = { "ruby", "typescript" },
       }),
     },
   })
+
+  local mappings = {
+    ["<leader>nr"] = function()
+      neotest.run.run(vim.fn.expand("%"))
+    end,
+    ["<leader>ns"] = function()
+      for _, adapter_id in ipairs(neotest.run.adapters()) do
+        neotest.run.run({ suite = true, adapter = adapter_id })
+      end
+    end,
+    ["<leader>nn"] = neotest.run.run,
+    ["<leader>nd"] = function()
+      neotest.run.run({ strategy = "dap" })
+    end,
+    ["<leader>nl"] = neotest.run.run_last,
+    ["<leader>nD"] = function()
+      neotest.run.run_last({ strategy = "dap" })
+    end,
+    ["<leader>na"] = neotest.run.attach,
+    ["<leader>no"] = function()
+      neotest.output.open({ enter = true })
+    end,
+    ["<leader>nO"] = function()
+      neotest.output.open({ enter = true, short = true })
+    end,
+    ["<leader>np"] = neotest.summary.toggle,
+    ["<leader>nm"] = neotest.summary.run_marked,
+    ["[n"] = function()
+      neotest.jump.prev({ status = "failed" })
+    end,
+    ["]n"] = function()
+      neotest.jump.next({ status = "failed" })
+    end,
+  }
+
+  for keys, mapping in pairs(mappings) do
+    vim.api.nvim_set_keymap("n", keys, "", { callback = mapping, noremap = true })
+  end
+end
+
+return M
