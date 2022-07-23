5008096d2f1d3e684af332269da78ef4a287cd86
Nevio <neviogomez91@gmail.com>
2022-07-23 14:28:53 -0400

    Updated icons

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -33,7 +33,7 @@ local function footer()
   -- local fortune = handle:read("*a")
   -- handle:close()
   -- return fortune
-  return "🚀 ngzportfolio.vercel.app"
+  return "爵 ngzportfolio.vercel.app"
 end
 
 dashboard.section.footer.val = footer()
